492bf58ec006ca0ee6056f84c78b6b08752da060
tejaltatiwari <tejaltatiwar487@gmail.com>
2024-06-20 18:27:49 +0530

    episode 1

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 // const heading = React.createElement("h1",{id:"heading"},"Hello World using React");
 //  const root=ReactDOM.createRoot(document.getElementById("root"));
 //  root.render(heading);
